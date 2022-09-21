05df8c367a3e088c71df934c9bda5de6a1484243
dquitor630 <dquitor630@g.educaand.es>
2022-09-21 10:36:21 +0200

    pequeño cambio en el 4

--- src/activity4/Activity4.java
+++ src/activity4/Activity4.java
@@ -8,18 +8,16 @@ public class Activity4 {
     public static void main(String[] args){
         new Activity4().pruebas();
     }
-
     void pruebas(){
         File dir = requestDirectory();
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Por favor, inserte la extensión sin punto que desea buscar");
-        String a = keyboard.nextLine();
-        File[] validFiles = dir.listFiles((dir1, name) -> name.endsWith(a));
+        String extension = keyboard.nextLine();
+        File[] validFiles = dir.listFiles((dir1, name) -> name.endsWith(extension));
         for(File f : validFiles){
             System.out.println(f.getName());
         }
     }
-
     public File requestDirectory() {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("por favor, inserta la ruta del directorio al que desea acceder: ");
