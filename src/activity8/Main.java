47c4644d58f401a0d40cae4f5b2f78c1ac9e4e9a
dquitor630 <dquitor630@g.educaand.es>
2022-10-04 20:49:57 +0200

    Ejercicio 8 primera parte terminada a falta de pulir

--- src/activity8/Main.java
+++ src/activity8/Main.java
@@ -9,7 +9,7 @@ public class Main {
     void pruebas() throws IOException {
         ArrayList<Contacto> contactos = new ArrayList<>();
         Contacto contactoUno = new Contacto("Diego Quirós", 678239920, "Calle colon n62 p6", 11300, true, 10.0, LocalDate.of(2003, 5, 14));
-        Contacto contactoDos = new Contacto("Pepe Quirós", 612565487, "Calle pedrosa n62 p6", 12333, false, 0.0, LocalDate.of(1999, 11, 13));
+        Contacto contactoDos = new Contacto("Pepe Torres", 612565487, "Calle pedrosa n62 p6", 12333, false, 0.0, LocalDate.of(1999, 11, 13));
         contactos.add(contactoUno);
         contactos.add(contactoDos);
         generateFile(contactos);
