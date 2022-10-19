6f92864d483653cf058648ad232ec39c60d779d5
dquitor630 <dquitor630@g.educaand.es>
2022-10-19 08:36:17 +0200

    UTF arreglado

--- src/activity9/Utf.java
+++ src/activity9/Utf.java
@@ -4,11 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.time.LocalDate;
+import java.util.Formatter;
 import java.util.Scanner;
 
 public class Utf {
-    private final static int BYTES = 82;
+    private final static int BYTES = 58;
     private ConsoleInput console = new ConsoleInput(new Scanner(System.in));
+
 
     void pruebas() throws IOException {
         File file;
@@ -107,14 +109,21 @@ public class Utf {
         Contacto contacto = createContact();
         RandomAccessFile random = new RandomAccessFile(file, "rw");
         StringBuilder buffer1, buffer2;
-        buffer1 = new StringBuilder(contacto.getContactName().trim());
-        buffer1.setLength(15);
+        Formatter formatter, formatter2;
+        buffer1 = new StringBuilder();
         buffer2 = new StringBuilder(contacto.getAddress().trim());
+        formatter = new Formatter(buffer1);
+        formatter2 = new Formatter(buffer2);
+        formatter.format("%-15s", contacto.getContactName().trim());
+        formatter2.format("%-20s", contacto.getContactName().trim());
+        buffer1.setLength(15);
         buffer2.setLength(20);
         random.seek(file.length());
         random.writeByte((int) (file.length() / BYTES + 1));
         random.writeBoolean(true);
+        System.out.println(random.getFilePointer());
         random.writeUTF(buffer1.toString());
+        System.out.println(random.getFilePointer());
         random.writeUTF(buffer2.toString());
         random.writeInt(contacto.getPhone());
         random.writeInt(contacto.getPostalCode());
@@ -127,24 +136,29 @@ public class Utf {
         Contacto contacto;
         RandomAccessFile random = new RandomAccessFile(file, "rw");
         StringBuilder buffer1, buffer2;
-        boolean writed = false;
-        while (!writed && random.getFilePointer() < random.length()) {
-            random.readByte();
-            if (!random.readBoolean()) {
-                contacto = createContact();
-                buffer1 = new StringBuilder(contacto.getContactName());
-                buffer1.setLength(15);
-                buffer2 = new StringBuilder(contacto.getAddress());
-                buffer2.setLength(20);
-                random.seek(random.getFilePointer() - 1);
                 random.writeBoolean(true);
-                random.writeUTF(buffer1.toString());
-                random.writeUTF(buffer2.toString());
-                random.writeInt(contacto.getPhone());
-                random.writeInt(contacto.getPostalCode());
-                random.writeBoolean(contacto.isLease());
-                random.writeDouble(contacto.getLeaseQuantity());
-                writed = true;
+                Formatter formatter, formatter2;
+                boolean writed = false;
+                while (!writed && random.getFilePointer() < random.length()) {
+                    random.readByte();
+                    if (!random.readBoolean()) {
+                        contacto = createContact();
+                        buffer1 = new StringBuilder();
+                        buffer2 = new StringBuilder();
+                        formatter = new Formatter(buffer1);
+                        formatter2 = new Formatter(buffer2);
+                        formatter.format("%-15s", contacto.getContactName().trim());
+                        formatter2.format("%-20s", contacto.getContactName().trim());
+                        buffer1.setLength(15);
+                        buffer2.setLength(20);
+                        random.seek(random.getFilePointer() - 1);
+                        random.writeUTF(buffer1.toString());
+                        random.writeUTF(buffer2.toString());
+                        random.writeInt(contacto.getPhone());
+                        random.writeInt(contacto.getPostalCode());
+                        random.writeBoolean(contacto.isLease());
+                        random.writeDouble(contacto.getLeaseQuantity());
+                        writed = true;
             } else {
                 random.seek(random.getFilePointer() + BYTES - 2);
             }
@@ -201,6 +215,7 @@ public class Utf {
         RandomAccessFile randomCopy = new RandomAccessFile(fileCopy, "rw");
         StringBuilder buffer1, buffer2;
         String name, address;
+        Formatter formatter, formatter2;
         byte id = 1;
         while (random.getFilePointer() < random.length()) {
             random.readByte();
@@ -210,10 +225,12 @@ public class Utf {
                 randomCopy.writeBoolean(random.readBoolean());
                 name = random.readUTF();
                 address = random.readUTF();
-                buffer1 = new StringBuilder(name.trim());
-                buffer1.setLength(15);
-                buffer2 = new StringBuilder(address.trim());
-                buffer2.setLength(20);
+                buffer1 = new StringBuilder();
+                buffer2 = new StringBuilder();
+                formatter = new Formatter(buffer1);
+                formatter2 = new Formatter(buffer2);
+                formatter.format("%-15s", name.trim());
+                formatter2.format("%-20s", address.trim());
                 randomCopy.writeUTF(buffer1.toString());
                 randomCopy.writeUTF(buffer2.toString());
                 randomCopy.writeInt(random.readInt());
