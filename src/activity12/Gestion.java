e3f4020ab9bb9b6a611f0ca7b751b6c2e8a9ea2e
dquitor630 <dquitor630@g.educaand.es>
2022-11-04 12:47:59 +0100

    Ejercicio 12 definitivo

--- src/activity12/Gestion.java
+++ src/activity12/Gestion.java
@@ -1,42 +1,57 @@
 package activity12;
 
 import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Gestion extends DefaultHandler {
     File file;
     FileWriter writer;
+    String att;
+    String[] data;
+    int count = 0;
     public Gestion() throws IOException {
         super();
+        data = new String[7];
         file = new File("C:\\Users\\diego\\IdeaProjects\\ActivityAd1_1\\src\\activity12\\contactos.txt");
         writer = new FileWriter(file);
         file.createNewFile();
     }
 
     public void startElement(String namespaceURI, String localName, String qName, Attributes atts){
+        att = "";
         for(int i = 0; i < atts.getLength(); i++){
-            System.out.print(atts.getValue(i));
-            try {
-                writer.write(atts.getValue(i) + " ");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            att = "%s%s".formatted(att, atts.getValue(i));
         }
     }
 
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        String str = new String(ch, start, length);
-        System.out.print(str);
-        try {
-            writer.write(str);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void characters(char[] ch, int start, int length) {
+        StringBuilder str;
+        if(count < 7){
+            str = new StringBuilder(new String(ch, start, length));
+            if(!str.toString().isBlank()){
+                data[count] = att + " " + str.toString().replace("\n", "") + " ";
+                count++;
+            }
+        }else{
+            str = new StringBuilder();
+            for (String datum : data) {
+                str.append(datum);
+            }
+            str.append("\n");
+            try {
+                writer.write(String.valueOf(str));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            count = 0;
+            Arrays.fill(data, "");
         }
+
     }
 
     public void endDocument(){
